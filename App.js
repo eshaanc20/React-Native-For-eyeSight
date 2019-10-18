408bf5ea5679e507da6e17b31c508cb749d9316a
= <parshvashah414@gmail.com>
2019-10-17 22:04:36 -0400

    Added base64 configuration

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, Dimensions, Image} from 'react-native';
+import { Text, View, TouchableOpacity, Dimensions, Image } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 import { AuthSession } from 'expo';
 import axios from 'axios';
+import { Platform } from '@unimodules/core';
 
 export default class App extends React.Component {
   state = {
     CameraPermission: null,
-    uri: null
+    uri: null,
+    base64: null
   };
 
   async componentDidMount() {
@@ -17,9 +19,13 @@ export default class App extends React.Component {
   }
 
   picture = async () => {
-    const picture = await this.camera.takePictureAsync();
-    this.setState({uri: picture.uri});
-    axios.get('https://evening-waters-86048.herokuapp.com')
+    const picture = await this.camera.takePictureAsync(
+      (params = { base64: true })
+    );
+    this.setState({ base64: picture.base64 });
+    this.setState({ uri: picture.uri });
+    axios
+      .get('https://evening-waters-86048.herokuapp.com')
       .then(res => {
         alert(res.data);
       })
@@ -39,18 +45,26 @@ export default class App extends React.Component {
     } else {
       return (
         <View style={styles.container}>
-            {this.state.uri != null? 
-              <View style = {{width: 250, height: 400}} key={uri}>
-                <Image style={{width: 250, height: 400}} source={{uri}}/>
-              </View>: 
-              <Camera ref={ (ref) => {this.camera = ref} }>
+          {this.state.uri != null ? (
+            <View style={{ width: 250, height: 400 }} key={uri}>
+              <Image style={{ width: 250, height: 400 }} source={{ uri }} />
+            </View>
+          ) : (
+            <Camera
+              ref={ref => {
+                this.camera = ref;
+              }}
+            >
               <View style={styles.view}>
-                <TouchableOpacity style={styles.button} onPress={this.picture.bind(this)}>
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={this.picture.bind(this)}
+                >
                   <Text style={styles.text}>IDENTIFY</Text>
                 </TouchableOpacity>
               </View>
-              </Camera>
-            }
+            </Camera>
+          )}
         </View>
       );
     }
